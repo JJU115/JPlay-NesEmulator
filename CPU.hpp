442ca340cf3dad315a53fafe201755f9240cb586
Justin Underhay <jju115@telus.net>
2019-08-20 20:37:40 -0700

    Basic skeleton of CPU.

--- CPU.hpp
+++ CPU.hpp
@@ -2,5 +2,21 @@
 
 class CPU {
     public:
-        short RAM[2048];
+        void RUN();
+        char PULSE(char MODE);
+        CPU():ACC(0), IND_X(0), IND_Y(0), STAT(0x34), STCK_PNT(0xFD) {}
+    private:
+        char RAM[2048];
+        char ACC, IND_X, IND_Y, STAT, STCK_PNT;
+        short PROG_CNT;
 };
+
+
+void CPU::RUN() {
+
+}
+
+
+char CPU::PULSE(char MODE) {
+    return 0;
+}

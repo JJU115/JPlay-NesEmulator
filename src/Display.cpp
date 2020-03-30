f6c5dc2012be7184481aa7447b4b92362b79899a
Justin Underhay <jju115@telus.net>
2020-03-29 22:22:11 -0700

    Basic SDL code to display window and provide a pixel drawing function

--- src/Display.cpp
+++ src/Display.cpp
@@ -1,7 +1,21 @@
 #include "Display.hpp"
+#include <iostream>
 
-int main(int argv, char** args) {
+//Of course, should do error checks for the init, createWindow, and WindowSurface functions 
+Display::Display() {
+
+    SDL_Init(SDL_INIT_VIDEO);
+    pixels = new uint32_t[256 * 224]; //Holds RGBA values for each pixel on screen
+    nesWindow = SDL_CreateWindow("JPlay", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 256, 224, SDL_WINDOW_SHOWN);
+    nesRenderer = SDL_CreateRenderer(nesWindow, -1, 0);
+    nesTexture = SDL_CreateTexture(nesRenderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, 256, 224);
+}
 
 
-    return 0;
+void Display::RENDER_PIXEL(uint16_t num, uint32_t val) {
+    pixels[num] = val;
+    SDL_UpdateTexture(nesTexture, NULL, pixels, 256 * sizeof(Uint32));
+    SDL_RenderClear(nesRenderer); //All of this at the end of the update loop
+    SDL_RenderCopy(nesRenderer, nesTexture, NULL, NULL);
+    SDL_RenderPresent(nesRenderer);
 }
--- src/Display.hpp
+++ src/Display.hpp
@@ -2,8 +2,14 @@
 
 
 class Display {
+    friend class PPU;
     public:
-
+        Display();
     private:
+        void RENDER_PIXEL(uint16_t num, uint32_t val);
 
+        SDL_Window* nesWindow;
+        SDL_Renderer* nesRenderer;
+        SDL_Texture* nesTexture;
+        uint32_t* pixels;
 };
